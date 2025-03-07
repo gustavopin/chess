fc940e00596a4532e647dde206ad18ebd67cd198
gustavopin <gustavorp96@gmail.com>
2025-03-07 11:44:12 -0300

    branch criada para o xadrez no nivel Avancado

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 This program will simulate the basic movement of individual chess pieces.
 
-### Technical Information
+### Information
 
 The program starts with an interactive menu created by the the switch-case funcion, one for every piece in this simulation.
+
+The pieces have pre-estabilished movements:
+
+ - Tower moves 5 houses to the right;
+ - Bishop moves 5 to top right;
+ - Queen moves 8 to the left;
+ - Horse moves Top Right.
+
+For the movements of Tower and Queen there are void functions controling the recursive loop, the tower moving "Right" and the queen "Left".
+
+For bishop movement there is a nested loop that will print "Top Right" five times.
+
+Lastly, the horse movement uses a loop with an if function inside it. That way the loop will print "Top" 2 times and than, when the variable reaches the desired value, it prints "Right".
 
--- chess.c
+++ chess.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+//parameter for the tower movement
+void mov_torre(int houses){ //recursivo function and variable houses
+
+    if (houses > 0) //condition for the variable so it doesnt go lower than 0
+    {
+        printf("Right \n", houses); //prints the movement of the tower
+        mov_torre(houses - 1); //decreases the atribute of the variable to reach a break point
+    }
+}
+
+//parameter for the queen movement
+void mov_queen(int houses){ //recursivo function and variable houses
+
+    if (houses > 0) //condition for the variable so it doesnt go lower than 0
+    {
+        printf("Left \n", houses); //prints the movement of the queen
+        mov_queen(houses - 1); //decreases the atribute of the variable to reach a break point
+    }
+}
+
 int main(){
     
     //variables
-    int tower_mov, bishop_mov, queen_mov; //variables for the chess piece that will store the movement
-    char tower_dir[20], bishop_dir[20], queen_dir[20], horse_dir1[20], horse_dir2[20]; //variable to store the direction of the movement
     int chess_piece;
 
     //menu for choosing what piece to move
@@ -21,85 +39,45 @@ int main(){
     {
     case 1:
         //tower movement
-        //for inside a if in case the movement is bigger than 8
-        //if tower_mov <= 8 the code will show you the selected movement
-        //if tower_mov > 8 the code will stop and show you the message
 
-        printf("\nNumber of movements for the tower: ");
-        scanf("%d", &tower_mov);
-        printf("\nDirection of movement for the tower(up, down, left or right): ");
-        scanf(" %[^\n]", &tower_dir); //stores the direction of the movement with spaces
+        mov_torre(5); //add a limit to the movement of the piece
 
-        if(tower_mov < 9){
-            for (int i = 0; i < tower_mov; i++)
-            {
-                printf("\n%s", tower_dir);        
-            }  
-        } else{
-            printf("\nmovement is too long");   
-        }
-        break;
+    break;
 
     case 2:
-        //bishop movement
-        //same thing as the tower (for now)
+        //bishop movement (5 houses)
 
-        printf("\nNumber of movements for the bishop: ");
-        scanf("%d", &bishop_mov);
-        printf("\nDirection of movement for the bishop (down left, up left, down right or up right): ");
-        scanf(" %[^\n]", &bishop_dir); //stores the direction of the movement with spaces
-
-        if(bishop_mov < 9){
-            for (int i = 0; i < bishop_mov; i++)
+        for (int i = 5; i > 0; i--)
+        {
+            printf("Top");
+            for (int j = 0; j < 1; j++) 
             {
-                printf("\n%s", bishop_dir);        
-            }  
-        } else{
-            printf("\nmovement is too long");   
+                printf(" Right \n");
+            }   
         }
+        
         break;
 
     case 3:
         //queen movement
-        //same thing as the tower (for now)
 
-        printf("\nNumber of movements for the queen: ");
-        scanf("%d", &queen_mov);
-        printf("\nDirection of movement for the quenn (up, down, left, right, down left, up left, down right or up right): ");
-        scanf(" %[^\n]", &queen_dir); //stores the direction of the movement with spaces
+        mov_queen(8); //add a limit to the movement of the piece
 
-        if(queen_mov < 9){
-            for (int i = 0; i < queen_mov; i++)
-            {
-                printf("\n%s", queen_dir);        
-            }  
-        } else{
-            printf("\nmovement is too long");   
-        }
         break;
 
     case 4:
         //horse movement
-        printf("\nMovement for the horse: If up/down, second must be left or right. If Left/right, second must be up or down");
-        printf("\nFirst Direction of movement for the horse (up, down, left or right): ");
-        scanf(" %s", horse_dir1); //stores the direction of the movement with spaces
-        printf("\nSecond Direction of movement for the horse (up, down, left or right): ");
-        scanf(" %s", horse_dir2); //stores the direction of the movement with spaces
-
-        if(strcmp(horse_dir1, horse_dir2) != 0){ //this lines compares 2 strings because the second movement cannot have the same direction as the first movement
-            for (int i = 0; i < 2; i++) //create a variable i for the external loop that will print the first direction of movement while i is lower than 2
+    
+        for (int i = 2; i > 0 ; i--)
+        {
+            printf("Top\n");
+            if (i == 1)
             {
-                printf("%s\n", horse_dir1); //the first direction comes before the second, so the print must be before the second loop
-
-                for (int j = 0; j < i; j++) //creates a variable j that iquals 0, and will print the second direction of movement while j is lower than i
-                {
-                    printf("%s", horse_dir2);
-                }
-                
+                printf("Right");
             }
-        } else{
-            printf("Movement can't be done");
         }
+        
+        
         break;
 
     default:
